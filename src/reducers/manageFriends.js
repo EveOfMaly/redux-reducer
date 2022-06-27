e79c342e55d84fa412b0ab669b880af2d692aba9
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-06-27 11:38:02 -0700

    commit tree 61e26eedf0a3b13fc90f64aeb1c6076f2f0ba403

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,34 @@
-export function manageFriends(state, action){
-}
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return({
+                ...state,
+                friends: [
+                    ...state.friends,
+                     action.friend
+                ]}
+            )
+        case "REMOVE_FRIEND":
+            const friendIndex = state.friends.findIndex(friend => friend.id === action.id)
+            return(
+                {
+               friends: [
+                ...state.friends.slice(0, friendIndex),
+                ...state.friends.slice(friendIndex + 1)
+            ]}
+            )
+        default: 
+            return state;
+        }
+    }
+
+
+
+
+
+
+
+
+
+    //return a new value with the key value pairs of state carried over to a new object
+    //with friends overwrriten with an array that contains all the existing array elements (empty array), and update it to value of action of array.
